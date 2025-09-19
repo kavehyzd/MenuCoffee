f99eac5a2e2659332a2c812f3893c4710c35979a
kaveh.yzd <kaveh.yzd@gmail.com>
2025-09-19 12:09:44 +0330

    Add Category Resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // برای استفاده از env
+    ConfigModule.forRoot({
+      isGlobal: true, // کل پروژه در دسترس باشه
+    }),
+    // اتصال به MongoDB با env
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        catch: console.log(configService.get<string>('MONGO_URI'))
+      
+      }),
+    }),
+
+    CategoriesModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+   app.useStaticAssets(join(__dirname, '..', 'uploads'));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 4000;
+
+  await app.listen(port);
 }
 bootstrap();
